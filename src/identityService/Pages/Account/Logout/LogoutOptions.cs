fa863ed104beaf53defebcd041c96049a7637f67
marqvardi <42220810+marqvardi@users.noreply.github.com>
2024-01-21 22:02:08 +0000

    Added nuget packages, added duende IDS

---
+++ src/identityService/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace identityService.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
